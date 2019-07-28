81846ab4eed0e2a31992c661a09cc28dc614f25c
Amy Fogelman <abfnj1@gmail.com>
2019-07-28 14:04:43 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,6 +11,12 @@ class Waiter
   end
  
  
+ def meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+  end
+ 
+ 
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
